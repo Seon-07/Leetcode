3a5797a0426e389e6fdfcfe5fab784c89a2f3a00
Seon <tjswlghks0722@naver.com>
2024-09-21 12:43:25 +0900

    Time: 4 ms (100%), Space: 44.3 MB (12.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
-        String r_numbers = new StringBuilder(x+"").reverse().toString();
-        return (x+"").equals(r_numbers);
+        int o_number = x;
+        int r_number = 0;
+        while(x != 0) {
+        	int e = x % 10;
+        	r_number = (r_number * 10) + e;
+        	x /= 10;
+        }
+        return o_number == r_number;
     }
 }
