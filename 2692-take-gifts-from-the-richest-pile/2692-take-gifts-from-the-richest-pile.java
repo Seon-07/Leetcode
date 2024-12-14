05e6eaf86c9f042446faf6b6f342ec834742627f
Seon <tjswlghks0722@naver.com>
2024-12-14 22:21:40 +0900

    Time: 70 ms (5.93%), Space: 43.6 MB (8.71%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,18 +1,17 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         List<Long> list = new ArrayList<>();
+		long result = 0;
 		for(int n : gifts) {
 			list.add((long)n);
+			result += n;
 		}
 		for(int i = 0; i < k; i++) {
 			long max = Collections.max(list);
 			int idx = list.indexOf(max);
 			double num = Math.sqrt(max);
 			list.set(idx, (long)num);
-		}
-		long result = 0;
-		for(long n : list) {
-			result += n;
+			result -= (max-num);
 		}
 		return result;
     }
